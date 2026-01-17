1db551bff114898be1311ab863142d18c0775fa0
abeerqwdq <abeersinghbedi@gmail.com>
2026-01-16 19:04:16 -0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,20 @@
+local HttpService = game:GetService("HttpService")
+local HWID = game:GetService("RbxAnalyticsService"):GetClientId()
+
+local KEY = getgenv().KEY
+if not KEY then
+    game.Players.LocalPlayer:Kick("No key provided")
+end
+
+local API = "https://kas-key-mrdlfubmg-abeerqwdqs-projects.vercel.app/api/check"
+local url = API.."?key="..KEY.."&hwid="..HWID
+
+local res = game:HttpGet(url)
+local data = HttpService:JSONDecode(res)
+
+if data.status == "ok" then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/abeerqwdq/kathub/refs/heads/main/main.lua"))()
+else
+    game.Players.LocalPlayer:Kick(data.msg or "Access denied")
+end
 
